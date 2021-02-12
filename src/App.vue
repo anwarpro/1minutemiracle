afbb002ebe15fb2ed9b9348e587ae60be59433fa
Anwar Hussen <anwar.hussen4@gmail.com>
2021-02-12 06:06:15 +0600

    hero valentine logo added

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,10 @@
         <!--        logo-->
         <div class="flex md:justify-around justify-center items-center">
           <div class="text-white">
-            <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                 xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path
-                  d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
-            </svg>
+            <img src="./assets/hero-valentines.png" class="h-16 w-16">
           </div>
           <div class="ml-1">
-            <h3 class="text-2xl font-semibold text-white">1 Minute Miracle</h3>
+            <h3 class="text-2xl font-semibold text-white">Hero Valentines</h3>
           </div>
         </div>
 
